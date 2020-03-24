41388b21b059a3e67c646a6c5e8369321894bf13
leonardons93 <43036983+leonardons93@users.noreply.github.com>
2020-03-24 11:43:19 -0300

    bd ok

--- app/rotas/produto.js
+++ app/rotas/produto.js
@@ -3,3 +3,12 @@ module.exports = function (app) {
         res.render("secao/produto.ejs");
     })
 }
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://teste:chucky@cluster0-o6aox.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const urisuper = "mongodb+srv://leonardo:wg199361LNS@cluster0-o6aox.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,16 +1,28 @@
-var mysql = require('mysql');
+async function main() {
+	const MongoClient = require('mongodb').MongoClient;
+	const uri = "mongodb+srv://leonardo:wg199361LNS@cluster0-o6aox.gcp.mongodb.net/test?retryWrites=true&w=majority";;
 
-var connMySQL = function(){
-	console.log('Conexao com bd foi estabelecida');
-	return mysql.createConnection({
-		host : 'localhost',
-		user : 'root',
-		password : '1234',
-		database : 'portal_noticias'
-	});
+	const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+	try {
+		// Connect to the MongoDB cluster
+		await client.connect();
+
+		// Make the appropriate DB calls
+		await listDatabases(client);
+
+	} catch (e) {
+		console.error(e);
+	} finally {
+		await client.close();
+	}
 }
 
-module.exports = function () {
-	console.log('O autoload carregou o módulo de conexão com bd');
-	return connMySQL;
-}
+main().catch(console.error);
+
+async function listDatabases(client) {
+	databasesList = await client.db().admin().listDatabases();
+
+	console.log("Databases:");
+	databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
